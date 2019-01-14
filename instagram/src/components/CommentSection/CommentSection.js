8e50ef2a86c6db6936e3759aa6706768f0a77663
KROHNICK <business.kanekperez@gmail.com>
2019-01-14 15:48:14 -0800

    Completed PostContainer components includiong styling

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 function CommentSection(props) {
-  return <div className="commentSection" />;
+  return (
+    <div className="commentSection">
+      <div className="likes">
+        <div className="likesIcon">
+          <i class="far fa-heart" />
+        </div>
+        <div className="commentsIcon">
+          <i class="far fa-comment" />
+        </div>
+      </div>
+      <div className="comments">
+        <strong>373 likes</strong>
+        <p>
+          <strong>philzcoffee</strong> We've got more than just delicious
+          coffees to offer at our shops. Don't forget to check out our selection
+          of locally sou <span>more</span>
+        </p>
+        <p>
+          <strong>biancasaurus_</strong> T-minus a month until Big/Little/GLil
+          coffee dates!!! <i class="fas fa-heart" />
+          <a>@tifanny_monroy</a>
+          <a>@msafransky</a>
+        </p>
+        <p>
+          <strong>martinseludo</strong>
+          <i class="fas fa-smile-beam" />
+        </p>
+      </div>
+      <div className="date">
+        <span>2 HOURS AGO</span>
+      </div>
+      <form>
+        <input type="text" name="comment" placeholder="Add a comment..." />
+        <div className="dots">
+          <i class="fas fa-ellipsis-h" />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CommentSection;
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -5,17 +5,16 @@ function PostContainer(props) {
   return (
     <div className="postContainer">
       <div className="user">
-        <div className="profilePic" />
-        <div className="profileName" />
+        <div className="profilePic">.</div>
+        <div className="profileName">
+          <strong>philzcoffee</strong>
+        </div>
       </div>
-      <img />
-      <div className="likes">
-        <div className="likesIcon" />
-        <div className="commentsIcon" />
-        <p />
-      </div>
-      <div className="comments">
-        <p />
+      <div className="photo">
+        {/* <img
+        src=""
+        alt="Toast Img"
+      /> */}
       </div>
       <CommentSection />
     </div>
